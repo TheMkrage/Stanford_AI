e7ce105f1e63d4cf1a560c6d2eb6aa17bd997b43
Matthew Krager <metoofarmer@yahoo.com>
2015-08-03 21:12:51 -0700

    Why was this even here?

--- VexWebCrawler/src/org/webcrawler/bytes/ByteCounter.java
+++
@@ -1,9 +0,0 @@
-package org.webcrawler.bytes;
-
-public class ByteCounter {
-	private static int bytes = 0;
-	public static void addBytes(String str) {
-		bytes += str.length();
-		System.out.println("Bytes Uploaded: " + bytes + " with new string: " + str);
-	}
-}
